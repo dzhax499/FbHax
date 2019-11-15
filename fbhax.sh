ee418637a1b2b4b460f9186a00ff01d3690e3955
dzhax499 <57004084+dzhax499@users.noreply.github.com>
2019-11-16 06:50:54 +0700

    Add files via upload

---
+++ fbhax.sh
@@ -0,0 +1,60 @@
+clear
+sleep 1
+echo "loading..."
+sleep 6
+clear
+echo "
+ _____  ____   __ __   ____  __ __ 
+|     ||    \ |  |  | /    ||  |  |
+|   __||  o  )|  |  ||  o  ||  |  |
+|  |_  |     ||  _  ||     ||_   _|
+|   _] |  O  ||  |  ||  _  ||     |
+|  |   |     ||  |  ||  |  ||  |  |
+|__|   |_____||__|__||__|__||__|__|
+                                   "
+echo
+echo "
+===================================
+Author =Mr.DzHax/daz113   IG/FB = Dzakirtsabit
+youtube =YtDzakir
+==================================="
+sleep 3
+echo ""
+read -p "Are you ready? [y] " yes;
+pkg install python2
+pkg install openssh
+pkg install git
+pip2 install requests
+echo "[+] Done."
+sleep 4
+echo "Tools ini jangan diPersalah gunakan!"
+sleep 1
+read -p "Dzhax$> " help;
+if [ $help = "Start" ]
+then
+    echo "
+    PERINGATAN : KESALAHAN ATAU KEJAHATAN ADALAH TANGGUNG JAWAB DARI PEMAKAI
+    GUNAKAN TOOLS UNTUK KEBAIKAN JANGAN DISALAH GUNAKAN :)"
+    sleep 3
+    echo "Starting..."
+    sleep 3
+    read -p "UserName: " user;
+    sleep 2
+    read -p "PassWord: " pass;
+    sleep 1
+    echo "Memasuki..."
+    sleep 5
+    echo "tunggu sebentar Nyet..."
+    sleep 1
+    echo "[+] Initializing.."
+    sleep 7
+    echo "[+] Loading Drive.."
+    sleep 5
+    echo "[+] Conection PID Drive"
+    sleep 7
+    echo "[!] Failed to load Drive!"
+else
+    echo -e '\e[0;31m'"Type An error!"
+    echo -e '\e[0;31m'"Type start untuk mulai Hacking "
+    echo -e '\e[0;32m'"silahkan masukan kembali"
+fi
